669512a2381b8d14165cf11c34f5f5d8bfceae38
Turkheim <rubenadrianoyarce@gmail.com>
2024-08-18 12:47:03 -0300

    updated Scale mechanics
    hitting the scales now spawns a broken scale and its more precise

--- Scripts/CharacterBody3D.gd
+++ Scripts/CharacterBody3D.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 @export var movement_speed = 5
 
 var canShoot = true
-var arrow_strength = 50
+var arrow_strength = 30
 
 var mouse_sensitivity = 700
 var gamepad_sensitivity := 0.075
--- Scripts/Scale.gd
+++ Scripts/Scale.gd
@@ -1,22 +1,26 @@
 extends StaticBody3D
 @onready var timer = $Timer
-
+@onready var dead_scale = load("res://Scene/scaleDead.tscn")
 #@onready var hit = $Hit
-var Dead = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if  Dead == true:
-		translate (Vector3.UP * delta * 20)
 
 func _on_area_3d_body_entered(body):
 	timer.start()
-	Dead = true
+
 	body.queue_free()
+	
+	var instance
+	instance = dead_scale.instantiate()
+	get_tree().get_current_scene().add_child(instance)
+	
+	instance.transform = (global_transform)
+	instance.apply_central_impulse(global_transform.basis.y * 10 )
 	
 func _on_timer_timeout():
 	queue_free()
