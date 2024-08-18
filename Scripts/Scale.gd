929873669d082ab47ac3271959e8dff220b48b30
Turkheim <rubenadrianoyarce@gmail.com>
2024-08-18 13:56:09 -0300

    Created_material_changer
    bug, the arrow changes all materials, but counts the hits

--- Scripts/Scale.gd
+++ Scripts/Scale.gd
@@ -1,26 +1,36 @@
 extends StaticBody3D
 @onready var timer = $Timer
 @onready var dead_scale = load("res://Scene/scaleDead.tscn")
-#@onready var hit = $Hit
 
+#@onready var hit = $Hit
+var lives = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var material = $ScaleMesh.mesh.surface_get_material(0)
+	material.albedo_color = Color.CYAN
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _on_area_3d_body_entered(body):
-	timer.start()
-
+	var material = $ScaleMesh.mesh.surface_get_material(0)
+	#timer.start()
 	body.queue_free()
+	
+	lives = lives +1
 	
 	var instance
 	instance = dead_scale.instantiate()
 	get_tree().get_current_scene().add_child(instance)
 	
 	instance.transform = (global_transform)
-	instance.apply_central_impulse(global_transform.basis.y * 10 )
-	
-func _on_timer_timeout():
-	queue_free()
+	instance.apply_central_impulse(global_transform.basis.y * 5 )
+	if lives == 1:
+		material.albedo_color = Color.YELLOW
+	elif lives == 2:
+		material.albedo_color = Color.ORANGE
+	elif lives == 3:
+		material.albedo_color = Color.RED
+	elif lives == 4:
+		queue_free()
+#func _on_timer_timeout():
+	#queue_free()
