923ff79921fbf0b56c55fd03de98893e34617079
Turkheim <rubenadrianoyarce@gmail.com>
2024-08-19 07:34:33 -0300

    fixed broken stuff

--- Scripts/pivot.gd
+++ Scripts/pivot.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-
+@export var dragon_speed = 12
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_y(deg_to_rad(12) * delta)
+	rotate_y(deg_to_rad(dragon_speed) * delta)
