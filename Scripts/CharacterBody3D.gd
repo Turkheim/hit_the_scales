050dd1796d71fe9deb6d96e2e3ac42a0dbb3fb4d
Turkheim <rubenadrianoyarce@gmail.com>
2024-08-19 07:28:57 -0300

    Revert "updated_dragon_speed"
    This reverts commit a09bb78a25ee937fa04cf93b12b7fc50b231f57f.

--- Scripts/CharacterBody3D.gd
+++ Scripts/CharacterBody3D.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 @export var movement_speed = 5
 
 var canShoot = true
-@export var arrow_strength = 25
+var arrow_strength = 30
 
 var mouse_sensitivity = 700
 var gamepad_sensitivity := 0.075
--- Scripts/pivot.gd
+++ Scripts/pivot.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
-@export var degrees_per_second = 18
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_y(deg_to_rad(degrees_per_second) * delta)
+	rotate_y(deg_to_rad(12) * delta)
