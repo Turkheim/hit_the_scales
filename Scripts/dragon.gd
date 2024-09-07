c554c178be6b37411ef226fb4a4f0ac10c70a15e
Turkheim <rubenadrianoyarce@gmail.com>
2024-09-07 01:45:59 -0300

    Finished Mechanics
    missing bow textures

--- Scripts/dragon.gd
+++ Scripts/dragon.gd
@@ -1,7 +1,7 @@
 extends StaticBody3D
 
 
-
+@onready var dragonExploding = preload("res://Scene/MAp/dragon_exploding.tscn")
 signal you_win
 
 #func _on_dragon_core_body_entered(body):
@@ -11,10 +11,13 @@ signal you_win
 
 func _on_death_timer_timeout():
 	#instantiate exploding dragon
-	#queue_free()
-	get_tree().change_scene_to_file("res://Scene/Levels/WinScreen.tscn")
-
-
+	var instance
+	instance = dragonExploding.instantiate()
+	get_tree().get_current_scene().add_child(instance)
+	instance.transform = (global_transform)
+	#get_tree().change_scene_to_file("res://Scene/Levels/WinScreen.tscn")
+	you_win.emit()
+	queue_free()
 func _on_scale_you_win():
 	win_sequence()
 
@@ -72,3 +75,12 @@ func _on_scale_12_you_win():
 
 func _on_scale_13_you_win():
 	win_sequence()
+
+
+	
+func _on_game_dragon_attack():
+	$Dragon/AnimationPlayer.play("AttackDragon")
+	$AttackTimer.start()
+	$DragonSound.play()
+func _on_attack_timer_timeout():
+	$Dragon/AnimationPlayer.play("IdleDragon")
--- Scripts/gameCrossbow.gd
+++ Scripts/gameCrossbow.gd
@@ -10,7 +10,7 @@ var VillagesDestroyed = 0
 @onready var vfx5 = $Vfxs/Vfx5
 @onready var vfx6 = $Vfxs/Vfx6
 @onready var village_timer = $VillageTimer
-
+signal dragonAttack 
 func _ready():
 	village_timer.start()
 
@@ -18,6 +18,7 @@ func _on_village_timer_timeout():
 	if VillagesDestroyed == 0:
 		village_timer.start()
 		#destroy village 1 signal
+		dragonAttack.emit()
 		vfx1.process_mode = Node.PROCESS_MODE_INHERIT
 		VillagesDestroyed = VillagesDestroyed+1
 		print(VillagesDestroyed)
@@ -26,6 +27,7 @@ func _on_village_timer_timeout():
 		village_timer.wait_time = 12
 		village_timer.start()
 		#destroy village 2 signal
+		dragonAttack.emit()
 		vfx2.process_mode = Node.PROCESS_MODE_INHERIT
 		VillagesDestroyed = VillagesDestroyed+1
 		print(VillagesDestroyed)
@@ -34,6 +36,7 @@ func _on_village_timer_timeout():
 		village_timer.wait_time = 9
 		village_timer.start()
 		#destroy village 3 signal
+		dragonAttack.emit()
 		vfx3.process_mode = Node.PROCESS_MODE_INHERIT
 		VillagesDestroyed = VillagesDestroyed+1
 		print(VillagesDestroyed)
@@ -41,6 +44,7 @@ func _on_village_timer_timeout():
 	elif VillagesDestroyed == 3:
 		village_timer.wait_time = 12
 		village_timer.start()
+		dragonAttack.emit()
 		#destroy village 4 signal
 		vfx4.process_mode = Node.PROCESS_MODE_INHERIT
 		VillagesDestroyed = VillagesDestroyed+1
@@ -49,6 +53,7 @@ func _on_village_timer_timeout():
 	elif VillagesDestroyed == 4:
 		village_timer.wait_time = 9
 		village_timer.start()
+		dragonAttack.emit()
 		#destroy village 5 signal
 		vfx5.process_mode = Node.PROCESS_MODE_INHERIT
 		VillagesDestroyed = VillagesDestroyed+1
@@ -57,6 +62,7 @@ func _on_village_timer_timeout():
 	elif VillagesDestroyed == 5:
 		village_timer.wait_time = 3
 		village_timer.start()
+		dragonAttack.emit()
 		#destroy village 6 signal and lock weapon
 		vfx6.process_mode = Node.PROCESS_MODE_INHERIT
 		VillagesDestroyed = VillagesDestroyed+1
@@ -66,6 +72,9 @@ func _on_village_timer_timeout():
 	
 	elif VillagesDestroyed == 6:
 		get_tree().change_scene_to_file("res://Scene/Levels/LoseScreen.tscn")
+		
+func _on_dragon_you_win():
+	$Timer.start()
 
-
-
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://Scene/Levels/WinScreen.tscn")
